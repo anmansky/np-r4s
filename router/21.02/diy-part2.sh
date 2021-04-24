8c88dd6d84c97f9798bbc2851f82143835b89fb4
anmansky <81860980+anmansky@users.noreply.github.com>
2021-04-24 17:15:04 +0300

    Add files via upload

---
+++ router/21.02/diy-part2.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#========================================================================================================================
+# https://github.com/anmansky/np-r4s
+# Description: Automatically Build OpenWrt for Nanopi R4s
+# Function: Diy script (After Update feeds, Modify the default IP, hostname, theme, add/remove software packages, etc.)
+# Copyright (C) 2020 https://github.com/P3TERX/Actions-OpenWrt
+# Copyright (C) 2020 https://github.com/anmansky/np-r4s
+#========================================================================================================================
+
+rm -f target/linux/rockchip/patches-5.4/992-rockchip-rk3399-overclock-to-2.2-1.8-GHz-for-NanoPi4.patch
+# rm -f target/linux/rockchip/patches-5.10/992-rockchip-rk3399-overclock-to-2.2-1.8-GHz-for-NanoPi4.patch
+# rm -f target/linux/rockchip/patches-5.10/991-arm64-dts-rockchip-add-more-cpu-operating-points-for.patch
+rm -f target/linux/rockchip/patches-5.4/991-arm64-dts-rockchip-add-more-cpu-operating-points-for.patch
+
+# Modify default IP（FROM 192.168.1.1 CHANGE TO 10.0.10.100）
+sed -i 's/192.168.1.1/10.0.10.100/g' package/base-files/files/bin/config_generate
+# Modify default theme（FROM uci-theme-bootstrap CHANGE TO luci-theme-material）
+# sed -i 's/luci-theme-bootstrap/luci-theme-material/g' ./feeds/luci/collections/luci/Makefile
+# Modify default root's password（FROM 'password'[$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.] CHANGE TO 'your password'）
+# sed -i 's/root::0:0:99999:7:::/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.:0:0:99999:7:::/g' /etc/shadow
+
+# Mydiy-luci-app-and-theme（use to /.config luci-app&theme）
+# ==========luci-app-url==========
+# git clone https://github.com/kenzok8/openwrt-packages.git package/openwrt-packages
+# ==========luci-theme-url==========
+# git clone https://github.com/openwrt-develop/luci-theme-atmaterial.git package/luci-theme-atmaterial
+# svn co https://github.com/project-openwrt/openwrt/trunk/package/ctcgfw/luci-theme-edge package/luci-theme-edge
+
